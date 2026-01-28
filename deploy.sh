989fb2013e5f6eea700c4370d02aad563f850da5
Harshavoleti <mailharshavoleti@gmail.com>
2026-01-28 22:28:08 +0530

    Add VitePress configuration and Netlify deployment settings

--- deploy.sh
+++ deploy.sh
@@ -47,6 +47,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Blog",
   description: "A VitePress blog",
+  cleanUrls: true,
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
